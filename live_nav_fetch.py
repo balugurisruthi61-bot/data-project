cda2137653b192e43d9df374df38b5b5aeec97bc
Sruthi baluguri <balugurisruthi61@gmail.com>
2026-06-04 15:13:18 +0530

    Day 1: Data ingestion complete

---
+++ live_nav_fetch.py
@@ -0,0 +1,23 @@
+import requests
+import pandas as pd
+
+schemes = {
+    "SBI_Bluechip": 119551,
+    "ICICI_Bluechip": 120503,
+    "Nippon_Large_Cap": 118632,
+    "Axis_Bluechip": 119092,
+    "Kotak_Bluechip": 120841
+}
+
+for name, code in schemes.items():
+    url = f"https://api.mfapi.in/mf/{code}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    pd.DataFrame(data["data"]).to_csv(
+        f"data/raw/{name}_nav.csv",
+        index=False
+    )
+
+    print(f"{name} saved")
